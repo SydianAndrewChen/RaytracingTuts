9bf05a41af3bfca6e440c6b5a0ffcada15fe7bf6
SydianAndrewChen <SydianAndrewChen@github.com>
2023-01-03 21:20:17 +0800

    section 8

--- main.cpp
+++ main.cpp
@@ -4,14 +4,23 @@
 #include "headers/hittable.h"
 #include "headers/hittable_list.h"
 #include "headers/sphere.h"
+#include "headers/camera.h"
 #include "headers/common.h"
 
 #include <iostream>
 
-Color ray_color(const Ray & r, const Hittable & world){
+Color ray_color(const Ray & r, const Hittable & world, int depth){
     HitRecord rec;
-    if (world.hit(r, 0, infinity, rec)){
-        return 0.5 * (rec.normal + Color(1,1,1));
+
+    // If we've exceeded the ray bounce limit, no more light is gathered.
+    if (depth <= 0)
+        return Color(0,0,0);
+
+    if (world.hit(r, 0.001, infinity, rec)){
+        // Point3D target = rec.p + rec.normal + random_in_unit_sphere();
+        // Point3D target = rec.p + rec.normal + random_unit_vector();
+        Point3D target = rec.p + rec.normal + random_in_hemisphere(rec.normal);
+        return 0.5 * ray_color(Ray(rec.p, target - rec.p), world, depth-1);
     }
     vec3 unit_direction = unit_vector(r.direction());
     auto t = 0.5*(unit_direction.y() + 1.0);
@@ -24,6 +33,8 @@ int main() {
     const float aspect_ratio = 16.0 / 9.0;
     const int image_width = 400;
     const int image_height = static_cast<int>(image_width / aspect_ratio);
+    const int samples_per_pixel = 100;
+    const int max_depth = 50;
 
     // World
     HittableList world;
@@ -31,14 +42,7 @@ int main() {
     world.add(make_shared<Sphere>(Point3D(0,-100.5,-1), 100));
 
     // Camera
-    auto viewport_height = 2.0;
-    auto viewport_width = aspect_ratio * viewport_height;
-    auto focal_length = 1.0;
-
-    auto origin = Point3D(0, 0, 0);
-    auto horizontal = vec3(viewport_width, 0, 0);
-    auto vertical = vec3(0, viewport_height, 0);
-    auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
+    Camera camera;
 
     // Render
 
@@ -47,11 +51,14 @@ int main() {
     for (int j = image_height-1; j >= 0; --j) {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
-            auto u = double(i) / (image_width - 1);
-            auto v = double(j) / (image_height - 1);
-            Ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
-            Color pixel_color = ray_color(r, world);
-            write_color(std::cout, pixel_color);
+            Color pixel_color(0, 0, 0);
+            for (int s = 0; s < samples_per_pixel; ++s) {
+                auto u = (i + random_double()) / (image_width-1);
+                auto v = (j + random_double()) / (image_height-1);
+                Ray r = camera.get_ray(u, v);
+                pixel_color += ray_color(r, world, max_depth);
+            }
+            write_color(std::cout, pixel_color, samples_per_pixel);
         }
     }
 
